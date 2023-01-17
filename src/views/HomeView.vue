c1a4e6744cecd40848ac5fc6365425eefc0920e1
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-01-17 16:38:45 +0800

    Cleaned unneeded html elements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import NewHomeComponent from "../components/NewHomeComponent.vue";
 </script>
 
 <template>
+  <header
+    class="bg-amber-400 shadow-md fixed w-full px-10 sm:px-12 py-4 top-0 z-10"
+  >
+    <p class="text-white">Appointmate</p>
+  </header>
   <main>
-    <TheWelcome />
+    <NewHomeComponent />
   </main>
 </template>
