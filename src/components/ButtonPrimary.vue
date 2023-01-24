677aa9d684b44847ea441f9258232b981cab8f95
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-01-24 10:39:09 +0800

    Update ButtonPrimary.vue

--- src/components/ButtonPrimary.vue
+++ src/components/ButtonPrimary.vue
@@ -1,1 +1,16 @@
-<template></template>
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: "Primary",
+  },
+});
+</script>
+<template>
+  <button
+    class="mt-2 rounded-lg border-2 border-transparent bg-amber-400 p-2 px-6 text-sm font-medium text-white transition-colors hover:bg-amber-500 active:bg-orange-600"
+    type="submit"
+  >
+    {{ props.text }}
+  </button>
+</template>
