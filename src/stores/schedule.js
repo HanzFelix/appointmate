a61ae2ff4edd4444358e8bc7018e7c941a7a5dd4
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-02-04 09:13:36 +0800

    Create schedule.js

---
+++ src/stores/schedule.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import { appointmateDB } from "../firebase";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy,
+  getDoc,
+} from "firebase/firestore";
+
+const schedulesRef = collection(appointmateDB, "schedules");
+
+export const useScheduleStore = defineStore("schedules", {
+  state: (xd) => ({}),
+  getters: {},
+  actions: {
+    async getAppointmentSchedules(appointment_id) {
+      console.log("here are the appointment schedules");
+    },
+  },
+});
